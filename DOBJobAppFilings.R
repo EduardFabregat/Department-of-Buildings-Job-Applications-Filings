c2477366197ebb70fba3a0e1d712fa4ff35b7265
EduardFabregat <45469244+EduardFabregat@users.noreply.github.com>
2020-10-07 13:45:54 -0400

    Update DOBJobAppFilings.R

--- DOBJobAppFilings.R
+++ DOBJobAppFilings.R
@@ -1,3 +1,194 @@
+library(tidyverse)
+
+df <- Historical_DOB_Permit_Issuance
+
+rm(Historical_DOB_Permit_Issuance)
+
+#What fraction of all construction permits in this data set correspond to renewed permits? 
+#The filing type is recorded under column 'Filing Status'. 
+#For first-time permits the value in this column is 'INITIAL' and for 
+#renewed permits it is 'RENEWAL'.
+
+colnames(df)
+
+table(df$`Filing Status`)
+
+all_num <- nrow(df)
+
+ren_num <- nrow(df[df$`Filing Status`== "RENEWAL",])
+
+ren_num/all
+
+ren <- df[df$`Filing Status`=="RENEWAL", ]
+
+#The filing type of a permit is recorded under column 'Filing Status'. 
+#For first-time permits the value in this column is 'INITIAL' and for renewed permits 
+#it is 'RENEWAL'. What kind of an owner the permit was issued to is recorded in 
+#the column "Owner's Business Type", for corporations the value is 'CORPORATION', 
+#for individuals it is 'INDIVIDUAL'. If you consider only renewed permits, 
+#what is the ratio of the number of permits issued to corporations to the number of 
+#permits issued to individuals?
+
+table(ren$`Owner's Business Type`)
+
+corp <- nrow(ren[ren$`Owner's Business Type`=="CORPORATION",])
+
+indi <- nrow(ren[ren$`Owner's Business Type`=="INDIVIDUAL", ])
+
+all_ren <- nrow(ren)
+
+corp/indi
+
+#Consider only those permits that were issued for more than 365 days. 
+#The date on which a permit was issued is recorded in column 'Issuance Date', 
+#the date the permit expires is in column 'Expiration Date'. 
+#What fraction of these at least year-long permits were issued in the 
+#borough with the highest number of such permits? 
+#The borough where the construction will take place is in column 'BOROUGH'.
+
+
+df$diff_days <- as.Date(df$`Expiration Date`, "%m/%d/%Y")-
+  as.Date(df$`Issuance Date`, "%m/%d/%Y")
+
+more_year <- df[df$diff_days > 365,]
+
+table(more_year$BOROUGH)
+
+max_permit <- max(table(more_year$BOROUGH))
+
+max_permit/nrow(more_year)
+
+
+#Limit your analysis to permits that were filed in 2010. 
+#The column recording the date of permit filing is called 'Filing Date'. 
+#For each ZIP code compute the ratio between the number of permits issued in 2010 
+#and number of residents in that ZIP code. Make sure to discard any ZIP codes with 
+#population lower than 1000 people. Note: you will need to use both the DOB permits 
+#dataset and the US Census dataset. In the DOB permits dataset, the ZIP code is 
+#recorded under column 'Postcode'. How many ZIP codes are outliers in terms of number 
+#of construction permits per number of people - more specificaly, for how many ZIP 
+#codes does this computed ratio exceed the mean by 
+#more than twice the standard deviation?
+
+df$year <- format(as.Date(df$`Filing Date`, format="%d/%m/%Y"),"%Y")
+
+twenty_ten <- df[df$year == 2010, ]
+
+census <- X2010_Census_Population_By_Zipcode_ZCTA_
+
+census2 <- census[census$`2010 Census Population` >= 1000,]
+
+twenty_ten2 <- twenty_ten[which(twenty_ten$Postcode %in% census2$`Zip Code ZCTA`),]
+
+twenty_ten2$forsum <- 1
+
+zip_codes <- aggregate(x = twenty_ten2[,"forsum"],
+                  by = list(twenty_ten2$Postcode), FUN = "sum")
+
+colnames(census2)[1] <- "postcode"
+colnames(zip_codes)[1] <- "postcode"
+colnames(zip_codes)[2] <- "permits"
+
+zip_codes$postcode <- as.character(zip_codes$postcode)
+
+zip_codes2 <- left_join(zip_codes, census2)
+
+zip_codes2$ratio <- zip_codes2$permits/zip_codes2$`2010 Census Population`
+
+mean_zipcodes <- mean(zip_codes2$ratio)
+sd_zipcodes <- sd(zip_codes2$ratio)*2
+
+outliers_finder <- mean_zipcodes+sd_zipcodes
+
+
+outliers <- zip_codes2[zip_codes2$ratio > outliers_finder,]
+
+nrow(outliers)
+
+#Observe how the number of issued permits changes accross the years. 
+#Limit your analysis to years 1990-2012 (both inclusive). 
+#What is the coefficient of determination (R squared) between the year 
+#a permit was issued and the number of issued permits that year? 
+#The column recording the date when a permit was issued is called 'Issuance Date'. 
+
+ninety_twenty <- df[df$year <= 2012 & df$year >= 1990, ]
+
+ninety_twenty$forsum <- 1
+
+num_permits <- aggregate(x = ninety_twenty[,"forsum"],
+                       by = list(ninety_twenty$year), FUN = "sum")
+
+num_permits$Group.1 <- as.numeric(num_permits$Group.1)
+
+summary(lm(forsum~Group.1, num_permits))$r.squared
+
+#Let's investigate how the number of construction jobs that start in 
+#the summer vs the winter changes across the years. 
+#The date of construction job start is recorded in column 'Job Start Date'. 
+#For every year, compute the ratio between the number of construction jobs that 
+#start in the peak of summer (in July and August) and the number of jobs that 
+#start in the peak of winter (in January and February). 
+#Again limit your analysis to years 1990-2012 (both inclusive). 
+#Find the year when this ratio was maximal - what was the value of this ratio for 
+#that year?
+
+ninety_twenty$month <- format(as.Date(ninety_twenty$`Filing Date`, format="%d/%m/%Y"),"%m")
+
+
+summer <- ninety_twenty[ninety_twenty$month == "07" | ninety_twenty$month == "08", ]
+winter <- ninety_twenty[ninety_twenty$month == "01" | ninety_twenty$month == "02", ]
+
+summer_year <- aggregate(x = summer[,"forsum"],
+                         by = list(summer$year), FUN = "sum")
+
+winter_year <- aggregate(x = winter[,"forsum"],
+                         by = list(winter$year), FUN = "sum")
+
+colnames(summer_year)[1] <- "year"
+colnames(winter_year)[1] <- "year"
+colnames(summer_year)[2] <- "summer_permits"
+colnames(winter_year)[2] <- "winter_permits"
+
+sum_win <- left_join(summer_year, winter_year)
+
+sum_win$ratio <- sum_win$summer_permits/sum_win$winter_permits
+
+max(sum_win$ratio)
+
+
+#If we look at how permit duration varies with the construction start date, 
+#it appears like jobs that start in November tend to have shorter permit duration. 
+#The date of construction job start is recorded in column 'Job Start Date'. 
+#The date on which a permit was issued is recorded in column 'Issuance Date', 
+#the date the permit expires is in column 'Expiration Date'. 
+#Calculate the chi-square test statistic for testing whether a permit is more 
+#likely to be issued for less than 60 days when construction job start date is November. 
+#Again limit your analysis to data from years 1990-2012 (both inclusive).
+
+ninety_twenty2 <- ninety_twenty[,c(26,27,28)]
+
+ninety_twenty2$month_start <- format(as.Date(ninety_twenty2$`Job Start Date`, format="%d/%m/%Y"),"%m")
+
+ninety_twenty2$diff_days <- as.Date(ninety_twenty2$`Expiration Date`, "%m/%d/%Y")-
+  as.Date(ninety_twenty2$`Issuance Date`, "%m/%d/%Y")
+
+ninety_twenty3 <- ninety_twenty2[ninety_twenty2$diff_days >= 0, ]
+
+ninety_twenty3 <- ninety_twenty3 %>% 
+  mutate(days = 
+           ifelse(ninety_twenty3$diff_days < 60, "sixty", "not_sixty"))
+
+ninety_twenty3 <- ninety_twenty3 %>% 
+  mutate(month = 
+           ifelse(ninety_twenty3$month_start == "11", "november", "not_november"))
+
+
+tbl <- table(ninety_twenty3$days, ninety_twenty3$month)
+
+chisq.test(tbl)
+
+
+
 options(stringsAsFactors = FALSE)
 
 DOB1 <- DOB_Job_Application_Filings[DOB_Job_Application_Filings$`Doc #` == "01", ]
